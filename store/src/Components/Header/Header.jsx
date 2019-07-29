f0d807ecd25ddacd946dde14fd86a77f821cea93
Dan Chirila <dan.chirila@softvision.ro>
2019-07-29 16:21:07 +0300

    first react commit

---
+++ store/src/Components/Header/Header.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './Header.css'
+import Logo from '../Logo/Logo';
+import Contact from '../Contact/Contact';
+
+class Header extends React.Component{
+    render(){
+        return( 
+        <div className="header-container">
+            <div className="header-top-container">
+                <Logo />
+                <Contact />
+            </div>
+        </div>
+        )       
+    }
+}
+
+
+export default Header;
