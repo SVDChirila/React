dfffde39966dda83f5cd3ab3dcb42c8ba4d14f67
Dan Chirila <dan.chirila@softvision.ro>
2019-07-31 17:44:15 +0300

    Dropdown menu is working

--- store/src/Components/_MAIN/Main/Main.jsx
+++ store/src/Components/_MAIN/Main/Main.jsx
@@ -8,8 +8,10 @@ class Main extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
+            isToggled: false,
             categories: {
-                Face: [
+                Face: {id: 1,
+                    listItems:[
                     "Creams & Gels",
                     "Patches",
                     "Emulstions",
@@ -19,21 +21,25 @@ class Main extends React.Component {
                     "Scrubs",
                     "Peeling",
                     "Masks"
-                ],
-                Body: [],
-                Foot: [],
-                Hands: [],
-                Hair: [],
-                Makeup: []
+                ]},
+                Body: {id:2,listItems:["Creams", "Gel Shower"]},
+                Foot: {id:3, listItems:["Cream"]},
+                Hands: {id:4, listItems:["Cream","Balsam"]},
+                Hair: {id:5, listItems:["Shampoo","Balsam","Conditioner"]},
+                Makeup: {id:6, listItems:["Mascara","Eyeliner"]}
             }
         }
     }
+
+
+
+       
 
     render(){
         return(
             <div className="content">
                 
-            <Categories categories={this.state.categories}/>
+            <Categories categories={this.state.categories} />
             </div>
             
         )
